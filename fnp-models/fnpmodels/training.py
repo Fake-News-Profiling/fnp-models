825490809f80acf0039f089391c634a08e324b89
Josh Hook <joshhook@live.co.uk>
2021-04-26 23:15:49 +0100

    -

--- fnp-models/fnpmodels/training.py
+++ fnp-models/fnpmodels/training.py
@@ -5,5 +5,4 @@ from fnpmodels.processing.parse import parse_dataset
 if __name__ == "__main__":
     x, y = parse_dataset("../datasets", "en")
     model = EnsembleBertModel(ScopedHyperParameters.from_json("fnpmodels/ensemble_bert_hp.json"))
-    print(model([x[0]]), y[0])
-    # model.train(x, y)
+    model.train(x, y)
